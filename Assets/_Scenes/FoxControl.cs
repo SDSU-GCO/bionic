c50835803055fdafc737297e09135047f1a2e3d8
jackdholm <jackdholm1@gmail.com>
2016-02-12 18:59:04 -0600

    Created some scripts

---
+++ Assets/_Scenes/FoxControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoxControl : MonoBehaviour
+{
+
+
+    private Rigidbody2D _Rigidbody2D;
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+}
