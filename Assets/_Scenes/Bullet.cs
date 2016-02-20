80c553aaa30512326384100a34b2e2ef84a95bb4
jackdholm <jackdholm1@gmail.com>
2016-02-19 19:55:25 -0600

    Character Demo
    Running, jumping, and shooting (selective fire) fox

---
+++ Assets/_Scenes/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    float timeLeft = 10.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
--- Assets/_Scenes/FoxControl.cs
+++ Assets/_Scenes/FoxControl.cs
@@ -3,18 +3,82 @@ using System.Collections;
 
 public class FoxControl : MonoBehaviour
 {
-
-
     private Rigidbody2D _Rigidbody2D;
-	// Use this for initialization
-	void Start ()
+
+    public Rigidbody2D laser;
+
+    public KeyCode jump;
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+    public float speed;
+    public float jumpForce;
+    public KeyCode shoot;
+    public KeyCode selectFire;
+    bool automatic;
+    float autoTimeElapsed;
+    [SerializeField] private LayerMask m_WhatIsGround;
+    private Transform m_GroundCheck;
+    private bool m_Grounded;
+    const float k_GroundedRadius = .5f;
+    private Transform m_CeilingCheck;   // A position marking where to check for ceilings
+    const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
+    // Use this for initialization
+    void Start ()
+    {
+        _Rigidbody2D = GetComponent<Rigidbody2D>();
+        m_GroundCheck = transform.Find("GroundCheck");
+        m_CeilingCheck = transform.Find("CeilingCheck");
+        automatic = false;
+        autoTimeElapsed = 0f;
+
+    }
+    // Update is called once per frame
+    void Update ()
     {
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+                m_Grounded = true;
+        }
 
-	}
+
+
+        if (Input.GetKey(jump) && m_Grounded)
+        {
+            _Rigidbody2D.AddForce(new Vector2(0, jumpForce));
+            m_Grounded = false;
+        }
+        else if (Input.GetKey(moveLeft))
+        {
+            _Rigidbody2D.velocity = new Vector2(speed * -1, _Rigidbody2D.velocity.y);
+        }
+        else if (Input.GetKey(moveRight))
+        {
+            _Rigidbody2D.velocity = new Vector2(speed, _Rigidbody2D.velocity.y);
+        }
+
+        if(Input.GetKeyDown(selectFire))
+        {
+            automatic = !automatic;
+        }
+
+        if (automatic && Input.GetKey(shoot))
+        {
+            autoTimeElapsed += Time.deltaTime;
+            if (autoTimeElapsed >= 0.09f)
+            {
+                autoTimeElapsed = 0f;
+                Rigidbody2D _laser = Instantiate(laser, transform.position, transform.rotation) as Rigidbody2D;
+                _laser.velocity = new Vector2(80f, 0);
+            }
+        }
+        if (Input.GetKeyDown(shoot) && !automatic)
+        {
+            Rigidbody2D _laser = Instantiate(laser, transform.position, transform.rotation) as Rigidbody2D;
+            _laser.velocity = new Vector2(80f, 0);
+            
+        }
+    }
 }
