6eb55d5eaf5b460727cd25a7ebccb44f1b3e7380
staebal <alexander.staebell@jacks.sdstate.edu>
2016-02-26 21:06:32 -0600

    Enemy pseudocode
    I would need to look into where functions would need to go and look into
    the specific API codes, but the logic of the enemy should all be there

--- Assets/_Scenes/Enemy.cs
+++ Assets/_Scenes/Enemy.cs
@@ -9,7 +9,36 @@ public class Enemy : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+	// to update timers
 	void Update () {
-	
+		// call fox detection
 	}
+
+	// to update physics
+	void FixedUpdate(){	
+		// call move
+    }
+
+	// fox detection
+		// calculate distance between fox and enemy
+		float distanceToFox = Distance(Vector2 a, Vector2 b);
+		// if in range of the fox and burst-fire rof timer is zero
+			// call fire at fox
+			// set burst-fire rof timer to two
+		// decrement burst-fire rof timer by one (look into proper way to do this)
+
+	// fire at fox
+		// initialize shot counter to 0
+		// initialize shot rof to 0
+		// while shots fired is less than three
+			// if shot rof is zero 
+				// spawn shot
+			// set shot rof to 0.0025 sec
+			// increment shot counter by one (look into proper way to do this)
+			
+	// move
+		// enemy moves in the direction it srarted facing
+		// if enemy collides with object tagged "vertical wall"
+			// reserve movement direction and continue moving in that direction
+		
 }
