280ac0ac081d3839ef38590bb6cd9d6eb257b822
jackdholm <jackdholm1@gmail.com>
2016-04-08 17:48:01 -0500

    Frog
    Added basic frog script.

---
+++ Assets/_Scenes/Frog.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Frog : MonoBehaviour 
+{
+	public int facing;
+	public LayerMask playerMask;
+	public LayerMask wallMask;
+	public float range;
+	Collider2D[] colliders;
+	RaycastHit2D hit;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Sighted ())
+		{
+
+		}
+	}
+
+	bool Sighted()
+	{
+		colliders = Physics2D.OverlapCircleAll(transform.position,range,playerMask);
+		if (colliders.Length > 0)
+		{
+			Debug.Log ("In range");
+			hit = Physics2D.Linecast (transform.position, colliders [0].transform.position, wallMask);
+			if (hit)
+			{
+				Debug.Log ("not seen");
+				return false;
+			} 
+			else
+			{
+				Debug.Log ("seen");	
+				return true;
+			}
+		}
+		else
+			return false;
+	}
+}
